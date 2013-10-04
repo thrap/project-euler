a5c88b69ddfb67d5fd219b23f28164bbabc5d889
Magnus S. Thrap <magnus.solheim@gmail.com>
2013-10-04 12:37:03 +0200

    Added observation to problem 222

---
+++ src/problems/Problem222.java
@@ -0,0 +1,21 @@
+package problems;
+
+public class Problem222 {
+	public static void main(String[] args) {
+		/**
+		 * Kan redusere problemet til Œ putte 21 sirkler med tangent til 2 parallelle rette linjer med avstand 50mm
+		 * 
+		 * f¿rste sirkel (med radius r1): tangent til h¿yre linje og enden av r¿ret. Koordinater (r1,r1)
+		 * 
+		 * andre sirkel: tangent til venstre linje og f¿rste sirkel. Koordinater (50-r1)
+		 * 
+		 * tredje sirkel: tangent til h¿yre linje og andre sirkel
+		 * 
+		 * osv
+		 * 
+		 * http://mathworld.wolfram.com/TangentCircles.html
+		 * 
+		 * http://en.wikipedia.org/wiki/Tangent_lines_to_circles
+		 */
+	}
+}
