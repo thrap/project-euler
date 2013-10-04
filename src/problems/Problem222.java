cc3153dc83fc7475f6c1f61ab0185fdc0ef94dfb
Magnus S. Thrap <magnus.solheim@gmail.com>
2013-10-04 16:46:56 +0200

    Probably solved problem 222 (unconfirmed)

--- src/problems/Problem222.java
+++ src/problems/Problem222.java
@@ -1,8 +1,5 @@
 package problems;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import utils.Point;
 import utils.T;
 
@@ -15,88 +12,42 @@ public class Problem222 {
 			this.center = center;
 			this.r = radius;
 		}
-		
-		public String toMathematica() {
-			return "Circle[{"+center.x+", "+center.y+"}, "+r+"]";
-		}
 	}
+	
+	/**
+	 * Se Problem222BaseCase for plotting
+	 */
 	public static void main(String[] args) {
-		/**
-		 * Kan redusere problemet til Œ putte 21 sirkler med tangent til 2 parallelle rette linjer med avstand 50mm
-		 * 
-		 * f¿rste sirkel (med radius r1): tangent til h¿yre linje og enden av r¿ret. Koordinater (r1,r1)
-		 * 
-		 * andre sirkel: tangent til venstre linje og f¿rste sirkel. Koordinater (50-r2, ?)
-		 * 
-		 * tredje sirkel: tangent til h¿yre linje og andre sirkel. Koordinater (r3, ?)
-		 * 
-		 * osv
-		 * 
-		 * http://mathworld.wolfram.com/TangentCircles.html
-		 * 
-		 * http://en.wikipedia.org/wiki/Tangent_lines_to_circles
-		 */
-		
 		T t = new T();
+		int[] rs = {50, 48, 46, 44, 42, 40, 38, 36, 34, 32, 30, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49};
 		
-		boolean left = true;
+		boolean left = false;
+		Circle lastCircle = new Circle(new Point(((left = !left) ? 100-rs[0] : rs[0]),rs[0]), rs[0]);
+		for (int i = 1; i < rs.length; i++) {
+			int r = rs[i];
+			double x = ((left = !left) ? 100-r : r);
+			lastCircle = new Circle(new Point(x, findY(lastCircle, r, x)), r);
+		}
 		
-		int r1 = 47;
-		int r2 = 30;
-		int r3 = 43;
-		int r4 = 37;
-		
-		
-		Circle circle1 = new Circle(new Point(((left = !left) ? 100-r1 : r1),r1), r1);
-		
-		double x2 = ((left = !left) ? 100-r2 : r2);
-		double y2 = findY(circle1, r2, x2, circle1.center.y, circle1.center.y+100);
-		
-		Circle circle2 = new Circle(new Point(x2, y2), r2);
-		
-		double x3 = ((left = !left) ? 100-r3 : r3);
-		double y3 = findY(circle2, r3, x3, circle2.center.y, circle2.center.y+100);
-		
-		Circle circle3 = new Circle(new Point(x3, y3), r3);
-		
-		double x4 = ((left = !left) ? 100-r4 : r4);
-		double y4 = findY(circle3, r4, x4, circle3.center.y, circle3.center.y+100);
-		
-		Circle circle4 = new Circle(new Point(x4, y4), r4);
-		
-		
-		System.out.println("Plott dette i mathematica :)");
-		System.out.println("Graphics[{Line[{{0, 0}, {0, 500}}], Line[{{100, 0}, {100, 500}}], " +
-				circle1.toMathematica() +", " +
-				circle2.toMathematica() +", " +
-				circle3.toMathematica()+", " +
-				circle4.toMathematica()+"}]");
-		
-		System.out.println(t);
+		double length = lastCircle.center.y+lastCircle.r;
+		System.out.println("Length: " + Math.round((length/1000)*1e6) + " micrometers " + t);
 	}
 
-	/**
-	 * denne b¿r v¾re bin¾rs¿k noobface
-	 */
-	private static double findY(Circle circle1,int r2, double x1, double minY, double maxY) {
-		if (maxY-minY < 0.0000000000001)
-			return minY;
+	
+	private static double findY(Circle circle, int r, double x) {
+		return findY(circle.r, circle.center.x, circle.center.y, r, x, circle.center.y, circle.center.y+100);
+	}
+	
+	private static double findY(int r1, double x0, double y0, int r2, double x1, double y1min, double y1max) {
+		if (y1max-y1min < 1e-9)
+			return y1min;
 		
-		int r1 = circle1.r;
-		Point center1 = circle1.center;
+		double y1 = (y1min+y1max)/2;
 		
-		
-		double avgY = (minY+maxY)/2;
-		double right = Math.pow((r1+r2), 2);
-		double left = Math.pow((center1.x-x1),2) + Math.pow((center1.y-avgY),2);
-		
-		System.out.println(right);
-		System.out.println(left);
-		
-		if (left < right) {
-			return findY(circle1, r2, x1, avgY, maxY);
+		if ((x0-x1)*(x0-x1) + (y0-y1)*(y0-y1) < (r1+r2)*(r1+r2)) {
+			return findY(r1, x0, y0, r2, x1, y1, y1max);
 		} else {
-			return findY(circle1, r2, x1, minY, avgY);
+			return findY(r1, x0, y0, r2, x1, y1min, y1);
 		}
 	}
 }
