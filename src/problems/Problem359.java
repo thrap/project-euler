6d1d5ab1ca553f8ca7fc319f62bffe0a837fa20d
Magnus S. Thrap <magnus.solheim@gmail.com>
2013-10-11 00:06:46 +0200

    Solved problem 359

---
+++ src/problems/Problem359.java
@@ -0,0 +1,39 @@
+package problems;
+
+import java.math.BigInteger;
+
+import utils.T;
+
+public class Problem359 {
+	public static void main(String[] args) {
+		// 71328803586048 = 2^27*3^12
+		
+		T t = new T();
+		BigInteger sum = BigInteger.ZERO;
+		for (int a = 0; a <= 27; a++) {
+			for (int b = 0; b <= 12; b++) {
+				long f = (long)Math.pow(2, a)*(long)Math.pow(3, b);
+				long r = (long)Math.pow(2, 27-a)*(long)Math.pow(3, 12-b);
+				sum = sum.add(P(f,r));
+			}
+		}
+		System.out.println(sum.mod(BigInteger.TEN.pow(8)) + " " + t);
+	}
+	
+	static BigInteger TWO = BigInteger.valueOf(2);
+	
+	private static BigInteger P(long f, long r) {
+		if (f == 1) {
+			return BigInteger.ONE.add(P(f+1,r-1));
+		}
+		if (r == 2) {
+			return ((f+r)%2 == 1 ? P(f, r-1).add(BigInteger.ONE) : P(f, r+1).subtract(TWO));
+		}
+		if (r == 1) {
+			BigInteger N = BigInteger.valueOf(f/2);
+			return (f % 2 == 0 ? TWO.multiply(N.pow(2)) : TWO.multiply(N).multiply(N.add(BigInteger.ONE)));
+		}
+		long k = (r-1)/2;
+		return P(f + 2*k, r-2*k).add(BigInteger.valueOf(((f+r)%2 == 1 ? k : -k)));
+	}
+}
