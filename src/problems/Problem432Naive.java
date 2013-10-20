672130732a9102dfeda7a04aa738c46d860cec26
Magnus S. Thrap <magnus.solheim@gmail.com>
2013-10-21 00:26:45 +0200

    Added naive approach to problem 432

---
+++ src/problems/Problem432Naive.java
@@ -0,0 +1,19 @@
+package problems;
+
+import utils.Euler;
+import utils.T;
+
+public class Problem432Naive {
+	public static void main(String[] args) {
+		T t = new T();
+		System.out.println(S(510510,(int)1e6) + " " + t);
+	}
+
+	private static long S(long n, int m) {
+		long sum = 0;
+		for (int i = 1; i <= m; i++) {
+			sum += Euler.phi(n*i);
+		}
+		return sum;
+	}
+}
