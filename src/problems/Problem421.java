44c7ce39917579ded6c60c3ad751842245ae57b9
Magnus S. Thrap <magnus.solheim@gmail.com>
2013-11-06 03:30:24 +0100

    Added observation to 421

---
+++ src/problems/Problem421.java
@@ -0,0 +1,8 @@
+package problems;
+
+public class Problem421 {
+	public static void main(String[] args) {
+		// x^15 = (1+x) (1-x+x^2) (1-x+x^2-x^3+x^4) (1+x-x^3-x^4-x^5+x^7+x^8)
+		// se på faktorer og blæst denna hæhæ
+	}
+}
