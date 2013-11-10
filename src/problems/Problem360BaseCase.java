71a469a0ad5ecd7462e44855ea859b6e3f47dcfe
Magnus S. Thrap <magnus.solheim@gmail.com>
2013-11-10 23:52:28 +0100

    Added count

--- src/problems/Problem360BaseCase.java
+++ src/problems/Problem360BaseCase.java
@@ -6,15 +6,17 @@ public class Problem360BaseCase {
 	public static void main(String[] args) {
 		long sum = 0;
 		int r = 45;
+		int count = 0;
 		for (int x = -r; x <= r; x++) {
 			for (int y = -r; y <= r; y++) {
 				for (int z = -r; z <= r; z++) {
 					if (x*x+y*y+z*z == r*r) {
+						count++;
 						sum += Math.abs(x)+Math.abs(y)+Math.abs(z);
 					}
 				}
 			}
 		}
-		System.out.println(sum);
+		System.out.println(sum + " " + count);
 	}
 }
