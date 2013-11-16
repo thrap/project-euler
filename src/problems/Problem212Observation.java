b102bcc5c0e5fabba4bfca0b19d41210f48ec638
Magnus S. Thrap <magnus.solheim@gmail.com>
2013-11-16 04:11:52 +0100

    Renaming

--- src/problems/Problem212Observation.java
+++ src/problems/Problem212Observation.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-public class Problem212 {
+public class Problem212Observation {
 	private static class Cuboid {
 		int x, y, z, dx, dy, dz;
 		
