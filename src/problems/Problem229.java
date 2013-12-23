765101dc3ceb3a6161b7dcd60b7284e2c2bc5cb3
Magnus S. Thrap <magnus.solheim@gmail.com>
2013-12-23 22:03:14 +0100

    Improved algorithm 229

--- src/problems/Problem229.java
+++ src/problems/Problem229.java
@@ -1,10 +1,6 @@
 package problems;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import utils.Euler;
 import utils.T;
@@ -17,82 +13,107 @@ public class Problem229 {
 	 * 
 	 * denne lager alt for stor stack. fix plx
 	 */
-	
-	static int limit = (int)Math.pow(10, 7);
+
+    private static class CandidateGenerator {
+        private List<Long> primes = new ArrayList<Long>();
+        public CandidateGenerator() {
+            for (long p : Euler.primeList((int)Math.sqrt(limit))) {
+                if (isCritical(p))
+                    primes.add(p);
+            }
+        }
+
+        private void generateCandidates(long n, int i, List<Long> candidates) {
+            if (i == primes.size() || primes.get(i)*n > limit) {
+                candidates.add(n);
+            } else {
+                long p = primes.get(i);
+
+                for (int j = 0; n*Math.pow(p, j) <= limit; j+=2) {
+                    generateCandidates(n * (long) Math.pow(p, j), i + 1, candidates);
+                }
+            }
+        }
+
+        private List<Long> getCandidates() {
+            List<Long> candidates = new ArrayList<Long>();
+
+            generateCandidates(1, 0, candidates);
+
+            return candidates;
+        }
+    }
+
+    private static class SpecialGenerator {
+        private List<Long> primes = new ArrayList<Long>();
+        public SpecialGenerator() {
+            for (long p : Euler.primeList(limit)) {
+                if (!isCritical(p))
+                    primes.add(p);
+            }
+        }
+
+        private void generateSpecials(long n, int i, List<Long> specials) {
+            if (i == primes.size() || primes.get(i)*n > limit) {
+                specials.add(n);
+            } else {
+                long p = primes.get(i);
+
+                for (int j = 0; n*Math.pow(p, j) <= limit; j++) {
+                    generateSpecials(n * (long) Math.pow(p, j), i + 1, specials);
+                }
+            }
+        }
+
+        public List<Long> getSpecials() {
+            List<Long> specials = new ArrayList<Long>();
+
+            generateSpecials(1, 0, specials);
+            specials.remove(0);
+            Collections.sort(specials);
+
+            return specials;
+        }
+    }
+
+	static int limit = (int)Math.pow(10, 6);
 	public static void main(String[] args) {
 		T t = new T();
-		for (Integer p : Euler.primeList((int)Math.sqrt(limit))) {
-			if (isCritical(p))
-				critical.add(p);
-		}
-		for (Integer p : Euler.primeList(limit)) {
-			if (!isCritical(p))
-				other.add(p);
-		}
-		
-		reccurse(1, 0);
-		
-		int ones = 0;
-		for (int special: specials) {
-			Map<Integer, Integer> factors = Euler.primeFactorMap(special);
-			if (factors.size() == 1)
-				ones++;
-			System.out.println(factors);
-		}
-		System.out.println("Ones: "+ones);
-		
-		System.out.println("generert antall: "+specials.size() + " " + t);
-		System.out.println("riktig antall  : "+75373);
-		
-	}
-	
-	static List<Integer> other = new ArrayList<Integer>();
-	static List<Integer> critical = new ArrayList<Integer>();
-	
-	static List<Integer> specials = new ArrayList<Integer>();
-	
-	private static void reccurse(long n, int i) {
-		if (i == critical.size() || critical.get(i)*n > limit) {
-			reccurse2(n, 0);
-		} else {
-			int p = critical.get(i);
-			
-			for (int j = 0; n*Math.pow(p, j) <= limit; j+=2) {
-				reccurse(n*(long)Math.pow(p, j), i+1);
-			}
-		}
-	}
 
-	private static void reccurse2(long n, int i) {
-		if (i == other.size() || other.get(i)*n > limit) {
-			if (isBruteSpecial((int)n)) { // denne er ganske treg
-				specials.add((int)n);
-				if (specials.size() % 1000 == 0)
-					System.out.println(specials.size());
-			}
-		} else {
-			int p = other.get(i);
-			for (int j = 0; n*Math.pow(p, j) <= limit; j++) {
-				reccurse2(n*(long)Math.pow(p, j), i+1);
-			}
-		}
-	}
+        List<Long> specials = new SpecialGenerator().getSpecials();
+        List<Long> candidates = new CandidateGenerator().getCandidates();
 
-	private static boolean isBruteSpecial(int n) {
+        int count = 0;
+        for (long a : candidates) {
+            if (isBruteSpecial(a))
+                count++;
+
+            //binærsøk her breh
+            for (long b : specials) {
+                if (b * a > limit)
+                    break;
+                count++;
+            }
+        }
+        System.out.println(count + " " + t);
+        System.out.println(specials.size());
+    }
+	
+	private static boolean isBruteSpecial(long n) {
 		return isSquareSum(n, 1) && isSquareSum(n, 2) && isSquareSum(n, 3) && isSquareSum(n, 7);
 	}
 	
-	private static boolean isSquareSum(int n, int D) {
-		for (int a = 1; a*a < n; a++) {
+	private static boolean isSquareSum(long n, int D) {
+		for (long a = 1; a*a < n; a++) {
 			if ((n-a*a) % D == 0 && Euler.isPerfectSquare((n-a*a)/D))
 				return true;
 		}
 		return false;
 	}
 
-	private static boolean isCritical(int p) {
-		return  (p % 7 == 3) || (p % 7 == 5) || (p % 7 == 6) || 
-				(p % 4 == 3) || (p % 8 == 5) || (p % 8 == 7) || 
+	private static boolean isCritical(long p) {
+		return  (p % 7 == 3) || (p % 7 == 5) || (p % 7 == 6) ||
+				(p % 4 == 3) || (p % 8 == 5) || (p % 8 == 7) ||
 				(p % 3 == 2); 
 	}
 }
