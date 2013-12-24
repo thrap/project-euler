06da91a2c2d183f972c89f7ff26057f1eea85358
Magnus S. Thrap <magnus.solheim@gmail.com>
2013-12-24 21:32:41 +0100

    Refactoring - adds help methods

--- src/problems/Problem143v2.java
+++ src/problems/Problem143v2.java
@@ -18,50 +18,24 @@ public class Problem143v2 {
             return "("+p + ", " + q+")";
         }
     }
+
+    static int limit = 120000;
     public static void main(String[] args) {
-        /**
-         * The angles ATC, ATB and BTC are all 120 degrees
-          */
+        List<Triangle> triangles = getTriangles();
+        Map<Long, List<Triangle>> triangleMap = getTriangleMap(triangles);
 
-        // skal finne alle kombinasjoner av trekanter der én vinkel er 120grader og alle lengder er integer
-
-
-        // finds triangles
-        List<Triangle> triangles = new ArrayList<Triangle>();
-        Map<Long, List<Triangle>> map = new HashMap<Long, List<Triangle>>();
-
-        int limit = 120000;
-        for(long p = 1; p <= limit; p++) {
-            if (p%1000 == 0)
-                System.out.println(p + " / "+limit);
-            for(long q = p+1; p+q <= limit; q++) {
-                if (Euler.isPerfectSquare(p*p+p*q+q*q)) {
-                    if (!map.containsKey(p)) {
-                        map.put(p, new ArrayList<Triangle>());
-                    }
-                    if (!map.containsKey(q)) {
-                        map.put(q, new ArrayList<Triangle>());
-                    }
-                    Triangle tri = new Triangle(p,q);
-                    map.get(p).add(tri);
-                    map.get(q).add(tri);
-                    triangles.add(tri);
-                }
-            }
-        }
-
-        // Finds triangles where sides match
+        // Finds combination of triangles where sides match
         Set<Long> values = new TreeSet<Long>();
         int i = 0;
         for(Triangle T1 : triangles) {
             if (++i % 1000 == 0)
                 System.out.println(i + " / " + triangles.size());
 
-            for(Triangle T2 : map.get(T1.p)) {
+            for(Triangle T2 : triangleMap.get(T1.p)) {
                 if (T2 == T1)
                     continue;
-                for(Triangle T3 : map.get(T1.q)) {
-                    if (T3 == T1)
+                for(Triangle T3 : triangleMap.get(T1.q)) {
+                    if (T3 == T1 || T2 == T3)
                         continue;
                     Set<Long> nums = new HashSet<Long>(Arrays.asList(T1.p, T1.q, T2.p, T2.q, T3.p, T3.q));
                     int uniques = nums.size();
@@ -83,5 +57,42 @@ public class Problem143v2 {
             sum+=value;
         }
         System.out.println(sum);
+    }
+
+    /**
+     * The angles ATC, ATB and BTC are all 120 degrees
+     */
+    private static List<Triangle> getTriangles() {
+        List<Triangle> triangles = new ArrayList<Triangle>();
+        for(long p = 1; p <= limit; p++) {
+            if (p%1000 == 0)
+                System.out.println(p + " / "+limit);
+            for(long q = p+1; p+q <= limit; q++) {
+                if (Euler.isPerfectSquare(p*p+p*q+q*q)) {
+                    triangles.add(new Triangle(p,q));
+                }
+            }
+        }
+        return triangles;
+    }
+
+    private static Map<Long, List<Triangle>> getTriangleMap(List<Triangle> triangles) {
+        Map<Long, List<Triangle>> map = new HashMap<Long, List<Triangle>>();
+
+        for(Triangle tri : triangles) {
+            long p = tri.p;
+            long q = tri.q;
+
+            if (!map.containsKey(p)) {
+                map.put(p, new ArrayList<Triangle>());
+            }
+            if (!map.containsKey(q)) {
+                map.put(q, new ArrayList<Triangle>());
+            }
+            map.get(p).add(tri);
+            map.get(q).add(tri);
+        }
+
+        return map;
     }
 }
