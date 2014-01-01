bbf0943550c7732934a678741978aa568a293fa8
Magnus S. Thrap <magnus.solheim@gmail.com>
2014-01-01 11:51:37 -0300

    Adds observation 451

--- src/problems/Problem451.java
+++ src/problems/Problem451.java
@@ -5,23 +5,19 @@ import utils.T;
 
 public class Problem451 {
     public static void main(String[] args) {
-        System.out.println("l(15) = "+l(15));
-        System.out.println("l(100) = "+l(100));
-        System.out.println("l(7) = "+l(7));
-
-        T t = new T();
+        /**
+         * m*m == 1 mod n
+         *
+         * m*m-1 == 0 mod n
+         *
+         * m*m - 1 = k*n
+         */
+        long limit = 100;
         long sum = 0;
-        for(int n = 3; n <= 100; n++) {
-            System.out.println(n + ": "+l(n));
-            sum+=l(n);
+        T t = new T();
+        for(long m = 2; m < limit; m++) {
+            System.out.println(m*m-1);
         }
         System.out.println(sum + " " + t);
-    }
-
-    private static long l(long n) {
-        for(long m = n-2; true; m--) {
-            if (m*m % n == 1)
-                return m;
-        }
     }
 }
