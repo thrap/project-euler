8ebb34e17be10b707b44bae7110940879beb4ed6
Magnus S. Thrap <magnus.solheim@gmail.com>
2014-01-02 18:24:40 -0300

    Added fitsBelow and fitsAbove

--- src/problems/Problem237.java
+++ src/problems/Problem237.java
@@ -1,11 +1,15 @@
 package problems;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem237 {
-    private static enum Direction {
+    private static enum Cell {
         A(" | ",
           " | "),
 
-        B("___",
+        B("_ _",
           "   "),
 
         C("_| ",
@@ -21,39 +25,58 @@ public class Problem237 {
           " | ");
 
         String[] draw;
-        Direction(String... draw) {
+        Cell(String... draw) {
             this.draw = draw;
+        }
+
+        public boolean fitsAbove(Cell o) {
+            return this.draw[1].charAt(1) == o.draw[0].charAt(1);
+        }
+
+        public boolean fitsBelow(Cell o) {
+            return o.fitsAbove(this);
         }
     }
 
-    public static final Direction A = Direction.A;
-    public static final Direction B = Direction.B;
-    public static final Direction C = Direction.C;
-    public static final Direction D = Direction.D;
-    public static final Direction E = Direction.E;
-    public static final Direction F = Direction.F;
+    public static final Cell A = Cell.A;
+    public static final Cell B = Cell.B;
+    public static final Cell C = Cell.C;
+    public static final Cell D = Cell.D;
+    public static final Cell E = Cell.E;
+    public static final Cell F = Cell.F;
 
     public static void main(String[] args) {
-        Direction[][] board = {
-            {B, B, E, F, B, B, B, E, F, E},
-            {F, E, D, C, F, B, B, C, A, A},
-            {A, A, F, E, D, B, B, B, C, A},
-            {C, D, C, D, B, B, B, B, B, C},
-        };
-
-        print(board);
+        List<Cell[]> board = Arrays.asList(new Cell[]{B, F, A, C}, new Cell[]{B, E, A, D});
+        List<Cell[]> possible = new ArrayList<Cell[]>();
+        for(Cell a : Cell.values()) {
+            if (!a.fitsBelow(B))
+                continue;
+            for(Cell b : Cell.values()) {
+                if (!b.fitsBelow(a))
+                    continue;
+                for(Cell c : Cell.values()) {
+                    if (!c.fitsBelow(b))
+                        continue;
+                    for(Cell d : Cell.values()) {
+                        if (!d.fitsBelow(c) || !d.fitsAbove(B))
+                            continue;
+                        Cell[] column = {a, b, c, d};
+                        possible.add(column);
+                    }
+                }
+            }
+        }
+        print(possible);
     }
 
-    private static void print(Direction[][] board) {
-        for(int j = 0; j < board.length; j++) {
-            for(int i = 0; i < board[0].length; i++) {
-                System.out.print(board[j][i].draw[0]);
+    private static void print(List<Cell[]> board) {
+        for(int j = 0; j < 4; j++) {
+            for(int k = 0; k <= 1; k++) {
+                for(int i = 0; i < board.size(); i++) {
+                    System.out.print(board.get(i)[j].draw[k]);
+                }
+                System.out.println();
             }
-            System.out.println();
-            for(int i = 0; i < board[0].length; i++) {
-                System.out.print(board[j][i].draw[1]);
-            }
-            System.out.println();
         }
     }
 }
