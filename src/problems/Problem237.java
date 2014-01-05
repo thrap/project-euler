e4ab3254f864ab74f3d4fa5374ce3dd2bafaa90e
Magnus S. Thrap <magnus.solheim@gmail.com>
2014-01-05 08:48:49 -0300

    Refactoring

--- src/problems/Problem237.java
+++ src/problems/Problem237.java
@@ -2,34 +2,30 @@ package problems;
 
 import problems.problem237.Column;
 import problems.problem237.ColumnGenerator;
+import utils.T;
 
-import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 public class Problem237 {
 
-    public static List<Column> starts = new ArrayList<Column>();
-    public static List<Column> middles = new ArrayList<Column>();
-    public static List<Column> ends = new ArrayList<Column>();
 
     private static class Board {
         private Column left;
         private Column right;
         private final int length;
-        BigInteger count;
+        long count;
 
         public Board(Column column) {
-            this.count = BigInteger.ONE;
+            this.count = 1;
             this.length = 0;
             this.left = column;
             this.right = column;
         }
 
         public Board(Board left, Board right) {
-            this.count = (left.count.multiply(right.count)).mod(BigInteger.TEN.pow(8));
+            this.count = (left.count*right.count) % MOD;
             this.length = left.length +right.length;
             this.left = (left.left.ID == 0 ? right.left : left.left);
             this.right = (right.right.ID == 0 ? left.right : right.right);
@@ -46,52 +42,59 @@ public class Problem237 {
         }
     }
 
+    private static Map<String, List<Column>> columns = new ColumnGenerator().getColumns();
+    private static long MOD = (long)Math.pow(10, 8);
+
     public static void main(String[] args) {
-        Map<String, List<Column>> lists = new ColumnGenerator().getColumns();
-        starts = lists.get("starts");
-        middles = lists.get("middles");
-        ends = lists.get("ends");
+        T t = new T();
+        System.out.println(T((long)Math.pow(10, 12)) + " "+t);
+    }
 
-        for(Column a : middles) {
-            addColumn(a, Arrays.asList(a));
-        }
+    private static long T(long n) {
+        List<Board> boards = getBoards(n);
 
-        long limit = (long)Math.pow(10, 12);
-
-        List<Board>[] boards = new ArrayList[(int)(Math.log(limit)/Math.log(2))+1];
-        boards[0] = new ArrayList<Board>();
-        for(Column a : middles) {
-            boards[0].add(new Board(a));
-        }
-
-        for (int i = 1; i < boards.length; i++) {
-            boards[i] = extendBoards(boards[i-1], boards[i-1]);
-        }
-
-        List<Board> board = new ArrayList<Board>(boards[0]);
-
-        long asd = limit-3;
-        for(int exp = boards.length; exp >= 0; exp--) {
-            double pow = Math.pow(2, exp);
-            if (pow <= asd) {
-                board = extendBoards(board, boards[exp]);
-                asd -= pow;
-            }
-        }
-        BigInteger sum = BigInteger.ZERO;
-
-        for (Column start : starts) {
-            for(Board middle : board) {
+        long sum = 0;
+        for (Column start : columns.get("starts")) {
+            for(Board middle : boards) {
                 if (!middle.fitsRigthOf(new Board(start)))
                     continue;
-                for (Column end : ends) {
+                for (Column end : columns.get("ends")) {
                     if (new Board(end).fitsRigthOf(middle)) {
-                        sum = sum.add(middle.count);
+                        sum += middle.count;
                     }
                 }
             }
         }
-        System.out.println(sum.mod(BigInteger.TEN.pow(8)));
+        return sum % MOD;
+    }
+
+    private static List<Board> getBoards(long n) {
+        List<Board>[] powBoards = getPowBoards(n);
+
+        List<Board> boards = new ArrayList<Board>(powBoards[0]);
+
+        long columnsLeft = n-3;
+        for(int exp = powBoards.length; exp >= 0; exp--) {
+            double pow = Math.pow(2, exp);
+            if (pow <= columnsLeft) {
+                boards = extendBoards(boards, powBoards[exp]);
+                columnsLeft -= pow;
+            }
+        }
+        return boards;
+    }
+
+    private static List<Board>[] getPowBoards(long n) {
+        List<Board>[] powBoards = new ArrayList[(int)(Math.log(n)/Math.log(2))+1];
+        powBoards[0] = new ArrayList<Board>();
+        for(Column a : columns.get("middles")) {
+            powBoards[0].add(new Board(a));
+        }
+
+        for (int i = 1; i < powBoards.length; i++) {
+            powBoards[i] = extendBoards(powBoards[i-1], powBoards[i-1]);
+        }
+        return powBoards;
     }
 
     private static List<Board> extendBoards(List<Board> lefts, List<Board> rights) {
@@ -101,32 +104,18 @@ public class Problem237 {
                 if (right.fitsRigthOf(left)) {
                     Board board = new Board(left, right);
                     boolean added = false;
-                    for(Board four : result) {
-                        if (four.equals(board)) {
-                            four.count = four.count.add(board.count);
+                    for(Board savedBoard : result) {
+                        if (savedBoard.equals(board)) {
+                            savedBoard.count += board.count;
                             added = true;
+                            break;
                         }
                     }
                     if (!added)
-                        result.add(new Board(left, right));
+                        result.add(board);
                 }
             }
         }
         return result;
-    }
-
-    static int count = 0;
-    private static void addColumn(Column last, List<Column> columns) {
-        if (columns.size() == 8) {
-            count++;
-        } else {
-            for(Column middle : middles) {
-                if (middle.fitsRightOf(last)) {
-                    List<Column> newColumns = new ArrayList<Column>(columns);
-                    newColumns.add(middle);
-                    addColumn((middle.ID == 0 ? last : middle), newColumns);
-                }
-            }
-        }
     }
 }
