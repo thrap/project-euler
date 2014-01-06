05882f5e288f3f6a2f0070ea0d40a16112d9cca7
Magnus S. Thrap <magnus.solheim@gmail.com>
2014-01-06 05:04:59 -0300

    Solved problem 167

--- src/problems/Problem167.java
+++ src/problems/Problem167.java
@@ -7,38 +7,59 @@ import java.util.*;
 public class Problem167 {
 
     public static void main(String[] args) {
-        /**
-         * ER KUN 2 PARTALL PER SEKVENS
-         *
-         * indeks for partall: n+3
-         *
-         * problem: finne periode
-         */
-
         T t = new T();
+        long index = (long)Math.pow(10, 11);
+        long sum = 0;
         for (int n = 2; n <= 10; n++) {
-            List<Integer> Us = Us(2, 2*n+1, 20000);
+            List<Integer> Us = Uz(n, 3000000);
 
-            System.out.println(Us.get(3+n));
+            int cycle = getCycle(n, Us);
+            int diff = Us.get(n + 4 + cycle) - Us.get(n + 4);
 
-            ytterste:
-            for (int offset = 1; offset < Us.size(); offset++) {
-                int start = 3+n+1;
-                int diff = Us.get(start+offset)-Us.get(start);
-                for(int i = start; i+offset < Us.size(); i++) {
-                    if (diff != Us.get(i+offset)-Us.get(i)) {
-                        break;
-                    }
-                    if (i +offset == Us.size()-1) {
-                        System.out.println(n+": "+offset);
-                        System.out.println("Us(n + "+offset+") = Us(n) + "+diff);
-                        break ytterste;
-                    }
+            long count = index/cycle;
+            long newIndex = index % cycle;
+            if (newIndex < n+4) {
+                count--;
+                newIndex+=cycle;
+            }
+
+            sum += Us.get((int) newIndex-1) + diff * count;
+            System.out.println(n + ": Us(n + "+cycle+") = Us(n) + "+diff);
+        }
+        System.out.println(sum + " " +t);
+    }
+
+    private static int getCycle(int n, List<Integer> us) {
+        for (int offset = 1; true; offset++) {
+            int start = n+4;
+            int diff = us.get(start+offset)- us.get(start);
+            for(int i = start; i+offset < us.size(); i++) {
+                if (diff != us.get(i+offset)- us.get(i)) {
+                    break;
+                }
+                if (i + offset == us.size()-1) {
+                    return offset;
                 }
             }
-//            System.out.println(Us);
         }
-        System.out.println(t);
+    }
+
+    public static List<Integer> Uz(int n, int limit) {
+        List<Integer> U = Us(2, 2*n+1, n+5);
+        Set<Integer> numbers = new HashSet<Integer>(U);
+
+        while(U.size() < limit) {
+            int last = U.get(U.size()-1);
+
+            for(int next = last+2; true; next+=2) {
+                if (numbers.contains(next - 2) ^ numbers.contains(next - U.get(n+3))) {
+                    U.add(next);
+                    numbers.add(next);
+                    break;
+                }
+            }
+        }
+        return U;
     }
 
     public static List<Integer> Us(int a, int b, int limit) {
@@ -46,7 +67,10 @@ public class Problem167 {
         U.add(a);
         U.add(b);
         Set<Integer> numbers = new HashSet<Integer>(U);
+
         while(U.size() < limit) {
+            if (U.size() % 10000 == 0)
+                System.out.println(U.size());
             int last = U.get(U.size()-1);
 
             int next;
