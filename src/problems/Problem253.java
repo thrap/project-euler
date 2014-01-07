a5caabce1e30579099a403e4b888354bf640bf7e
Magnus S. Thrap <magnus.solheim@gmail.com>
2014-01-07 10:05:55 -0300

    Adds monte-carlo simulation

--- src/problems/Problem253.java
+++ src/problems/Problem253.java
@@ -8,13 +8,37 @@ import java.util.*;
 
 public class Problem253 {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        int pieces = 10;
+        int[] arr = new int[pieces];
+        for(int i = 0; i<pieces; i++) {
+            arr[i] = i+1;
+        }
 
-        /**
-         * Bruke montecarlo kanskje?
-         */
+        monteCarlo(Arrays.copyOf(arr, arr.length), 2000000);
+        exact(Arrays.copyOf(arr, arr.length));
+    }
+
+    private static void monteCarlo(int[] arr, int iterations) {
         T t = new T();
         Map<Integer, Integer> count = new TreeMap<Integer, Integer>();
+        for(int i = 0; i < iterations; i++) {
+            if (i % (iterations / 100) == 0)
+                System.out.println((100*i)/iterations+"/100");
+            Euler.shuffle(arr);
+            int M = M(arr);
+            count.put(M, count.containsKey(M) ? count.get(M) + 1 : 1);
+        }
+
+        int total = 0;
+        for(int M : count.keySet()) {
+            total += M*count.get(M);
+        }
+        System.out.println("Estimate: "+new Rational(total, iterations).doubleValue() + " " + t);
+    }
+
+    private static void exact(int[] arr) {
+        T t = new T();
+                Map<Integer, Integer> count = new TreeMap<Integer, Integer>();
         do {
             int M = M(arr);
             count.put(M, count.containsKey(M) ? count.get(M) + 1 : 1);
@@ -27,7 +51,7 @@ public class Problem253 {
         }
         int possibilities = Euler.factorial(arr.length).intValue();
         Rational average = new Rational(total, possibilities);
-        System.out.println(average + " = "+average.doubleValue() + " " + t);
+        System.out.println("Exact: "+average + " = "+average.doubleValue() + " " + t);
     }
 
     private static class Segment {
