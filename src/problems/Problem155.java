8ed887c28504bf007ae9f3419131334bec08a21b
Magnus S. Thrap <magnus.solheim@gmail.com>
2014-01-09 08:20:29 -0300

    Solved problem 155

--- src/problems/Problem155.java
+++ src/problems/Problem155.java
@@ -1,20 +1,40 @@
 package problems;
 
-public class Problem155 {
-	public static void main(String[] args) {
-		int n = 3;
-		
-		recursion("",0, n);
-	}
+import utils.Rational;
 
-	private static void recursion(String string, int used, int n) {
-		if (used == n) {
-			System.out.println(string);
-			return;
-		}
-		for (int i = 2; used+i <= n; i++) {
-			recursion(string+" "+i+"P", used+i, n);
-		}
-		recursion(string+" S", used+1, n);
-	}
+import java.util.HashSet;
+import java.util.Set;
+
+public class Problem155 {
+
+	public static void main(String[] args) {
+		int n = 18;
+        Set<Rational>[] subunits = new HashSet[n+1];
+        for(int i = 0; i<subunits.length; i++) {
+            subunits[i] = new HashSet<Rational>();
+        }
+
+        subunits[1].add(new Rational(60,1));
+
+        for(int i = 2; i < subunits.length; i++) {
+            System.out.println(i);
+            for(int a = 1; a < i && i-a >= a; a++) {
+                int b = i-a;
+                for(Rational sub1 : subunits[a]) {
+                    for (Rational sub2 : subunits[b]) {
+                        subunits[i].add((sub1.multiply(sub2)).divide(sub1.add(sub2)));
+                        subunits[i].add(sub1.add(sub2));
+                    }
+                }
+            }
+        }
+
+        Set<Rational> values = new HashSet<Rational>();
+        for(int i = 0; i<subunits.length; i++) {
+            values.addAll(subunits[i]);
+        }
+
+        System.out.println(values.size());
+    }
+
 }
