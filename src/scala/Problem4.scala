ecee2f2e3037306de90c1bd6aa868bddd9e24fea
Magnus S. Thrap <magnus.solheim@gmail.com>
2014-03-09 05:03:43 +0100

    Uses list comprehentions instead of for loops

--- src/scala/Problem4.scala
+++ src/scala/Problem4.scala
@@ -1,11 +1,6 @@
 package scala
 
 object Problem4 extends App{
-  var max = 0
   def isPalindrome(x:String):Boolean = x.reverse.equals(x)
-  for(a <- 900 to 999; b <- a to 999) {
-    if (isPalindrome(""+a*b))
-      max = Math.max(a*b, max)
-  }
-  println(max)
+  print((for(a <- 900 to 999; b <- a to 999; if isPalindrome(""+(a*b))) yield a*b).max)
 }
