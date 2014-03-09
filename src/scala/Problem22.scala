4af70307031dbbee3a83804b947c8a0668616475
Magnus S. Thrap <magnus.solheim@gmail.com>
2014-03-10 00:43:42 +0100

    Fixes formatting

--- src/scala/Problem22.scala
+++ src/scala/Problem22.scala
@@ -3,6 +3,11 @@ package scala
 import scala.io.Source._
 
 object Problem22 extends App {
-  var names = fromFile("src/input-files/p22.txt").getLines().next().replace("\"","").split(",").sorted
-  println(names.map(name => name.map(_-'A'+1).sum).zipWithIndex.map(x => x._1*(x._2+1)).sum)
+  println(fromFile("src/input-files/p22.txt").getLines().next()
+    .replace("\"","").split(",")
+    .sorted
+    .map(name => name.map(_-'A'+1).sum)
+    .zipWithIndex
+    .map{ case(w, idx)=> w*(idx+1) }
+    .sum)
 }
