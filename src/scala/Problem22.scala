3ff20eae6acc9c7a828cdf5883fdb9d64f9e659b
Magnus S. Thrap <magnus.solheim@gmail.com>
2014-03-10 00:36:11 +0100

    Uses regex instead of substr

--- src/scala/Problem22.scala
+++ src/scala/Problem22.scala
@@ -3,6 +3,6 @@ package scala
 import scala.io.Source._
 
 object Problem22 extends App {
-  var names = fromFile("src/input-files/p22.txt").getLines().next().split(",").map(name => name.substring(1, name.length - 1)).sortWith(_ < _)
+  var names = fromFile("src/input-files/p22.txt").getLines().next().replace("\"","").split(",").sorted
   println(names.map(name => name.map(_-'A'+1).sum).zipWithIndex.map(x => x._1*(x._2+1)).sum)
 }
