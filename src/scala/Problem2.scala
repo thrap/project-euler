319df3b5bac62a853eb40a70e32f9c35ad196fe0
Magnus S. Thrap <magnus.solheim@gmail.com>
2014-03-09 14:22:56 +0100

    Removes brackets from one-liner function

--- src/scala/Problem2.scala
+++ src/scala/Problem2.scala
@@ -3,9 +3,6 @@ package scala
 import utils.Euler
 
 object Problem2 extends App{
-  def sum(n:Int, s:Long):Long = {
-    val fib = Euler.fib(n)
-    if(fib > 4e6) s else sum(n+3, s+fib)
-  }
+  def sum(n:Int, s:Long):Long = if(Euler.fib(n) > 4e6) s else sum(n+3, s+Euler.fib(n))
   println(sum(3,0))
 }
