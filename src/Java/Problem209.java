f69b1256f6eec6debf8955ded390ae8ee1ff5281
Magnus S. Thrap <magnus.solheim@gmail.com>
2014-06-19 17:40:06 +0200

    Solves problem 209

--- src/Java/Problem209.java
+++ src/Java/Problem209.java
@@ -1,32 +1,33 @@
 package Java;
 
-import java.util.HashSet;
-import java.util.Set;
+import utils.T;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Problem209 {
-	public static void main(String[] args) {
-		for (int a = 0; a <= 1; a++) {
-			for (int b = 0; b <= 1; b++) {
-				for (int c = 0; c <= 1; c++) {
-					for (int d = 0; d <= 1; d++) {
-						for (int e = 0; e <= 1; e++) {
-							for (int f = 0; f <= 1; f++) {
-								T(a,b,c,d,e,f);
-//								T(b,c,d,e,f,a^(b&c));
-							}
-						}
-					}
-				}
-			}
-		}
-		System.out.println(set.size());
-	}
-
-	static Set<String> set = new HashSet<String>();
-	private static void T(int a, int b, int c, int d, int e, int f) {
-		String bits = ""+a+b+c+d+e+f;
-		System.out.println(bits);
-		set.add(bits);
-	}
-
+    private static int N = 6;
+    private static long[] lists = new long[1 << N];
+    public static void main(String[] args) {
+        T t = new T();
+        for (int i = 0; i < (1<<N); i++) {
+            int j = (i>>(N-3))&(i>>(N-2))&1^(i>>(N-1))+(((1<<(N-1))-1)&i)*2;
+            lists[Math.min(i, j)] |= 1L << Math.max(i, j);
+        }
+        System.out.println(r(1, 0) + " " + t);
+    }
+    private static Map<Long, Long> cache = new HashMap<>();
+    public static long r(int i, long s) {
+        if (i == 1L << N)
+            return 1;
+        if (cache.containsKey(s))
+            return cache.get(s);
+        if ((s & 1L<<i) != 0) {
+            return r(i+1, s);
+        } else {
+            long sum = r(i+1, s | (1L << i)) + r(i+1, s | (1L << i) | lists[i]);
+            cache.put(s, sum);
+            return sum;
+        }
+    }
 }
