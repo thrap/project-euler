42465669fb2bee3e883f228f2727d2d33120deae
Magnus S. Thrap <magnus.solheim@gmail.com>
2014-08-08 17:41:06 +0200

    Refactor #114.scala

--- src/scala/Problem114.scala
+++ src/scala/Problem114.scala
@@ -1,19 +1,21 @@
 package scala
 
 object Problem114 extends App {
-  var cache = Array.fill[Long](100)(0)
 
-  def count(tiles: Int, width: Int): Long = {
-    if (cache(width) != 0)
-      return cache(width)
-
-    cache(width) = (
-      for (blanks <- 1 to tiles - width; blocks <- 3 to tiles - blanks - width)
-        yield 1 + count(tiles, width + blocks + blanks)
-      ).sum
-    cache(width)
+  def count(tiles: Int, min: Int): Long = {
+    val cache = Array.fill[Long](tiles+2)(0)
+    def count(tiles: Int, width: Int, min: Int): Long = {
+      if (cache(width) != 0)
+        return cache(width)
+      cache(width) = (
+        for (blanks <- 1 to tiles - width; blocks <- min to tiles - blanks - width)
+          yield 1 + count(tiles, width + blocks + blanks, min)
+        ).sum
+      cache(width)
+    }
+    count(tiles+1,0, min)+1
   }
 
-  println(count(51,0)+1)
+  println(count(50,3))
 
 }
