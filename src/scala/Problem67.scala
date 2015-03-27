932548d7f792ef0fc37b9d1c4096c788ac31a729
Magnus S. Thrap <magnus.solheim@gmail.com>
2015-03-28 00:10:29 +0100

    Solve #67 in scala

---
+++ src/scala/Problem67.scala
@@ -0,0 +1,14 @@
+package scala
+
+import scala.io.Source._
+
+object Problem67 extends App {
+  def biggest(board:Array[Array[Int]]):Int = {
+    for (y <- (0 to board.length-2).reverse)
+      for (x <- 0 until board(y).length)
+        board(y)(x) += Math.max(board(y+1)(x), board(y+1)(x+1))
+    board(0)(0)
+  }
+
+  print(biggest(fromFile("src/input-files/p67.txt").getLines().toArray.map(_.split(' ').map(_.toInt))))
+}
