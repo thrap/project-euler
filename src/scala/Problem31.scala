310d7134c787ba90a17e7c7c99193ef1b25c476e
Magnus S. Thrap <magnus.solheim@gmail.com>
2015-03-28 00:22:02 +0100

    Refactor #31

--- src/scala/Problem31.scala
+++ src/scala/Problem31.scala
@@ -1,12 +1,8 @@
 package scala
 
 object Problem31 extends App {
-  val nums = Array.fill(201){0}
-  val coins = List(1,2,5,10,20,50,100,200)
-  for (coin <- coins) {
-    nums(coin) += 1
-    for (i <- coin until nums.length)
-      nums(i) += nums(i-coin)
-  }
-  println(nums(200))
+  val p = Array.fill(201){1L}
+  for (n <- List(2,5,10,20,50,100,200); i <- n until p.length)
+    p(i) += p(i-n)
+  println(p(200))
 }
