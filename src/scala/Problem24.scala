dd3a436416c3760132c62ed2722075e9b9b0fe47
Magnus S. Thrap <magnus.solheim@gmail.com>
2015-03-29 23:53:53 +0200

    Refactor #24

--- src/scala/Problem24.scala
+++ src/scala/Problem24.scala
@@ -1,11 +1,11 @@
 package scala
 
 object Problem24 extends App {
-  def lexiPerm(list:List[Int], n:Long):String = {
+  def lexiPerm(list:List[Int], n:Int):String = {
     if (list.size == 0)
       return ""
-    val f = (1L to list.length-1L).product
-    list((n/f).toInt) + lexiPerm(list.patch((n/f).toInt,Nil,1).toList, n-(n/f)*f)
+    val f = (1 until list.length).product
+    list(n / f) + lexiPerm(list diff List(list(n / f)), n-(n/f)*f)
   }
 
   println(lexiPerm((0 to 9).toList, 1000000-1))
