b50183720667e4966df27be93472220197e715cf
Magnus S. Thrap <magnus.solheim@gmail.com>
2015-05-20 15:00:21 +0200

    Solve #358

--- src/Java/Problem358.java
+++ src/Java/Problem358.java
@@ -1,53 +1,51 @@
 package Java;
 
-import java.util.List;
-
 import utils.Euler;
 import utils.T;
 
+import static java.math.BigInteger.valueOf;
+
 
 public class Problem358 {
-	public static void main(String[] args) {
-		T t = new T();
-		int count = 0;
-		List<Long> primeListBetween = Euler.primeListBetween((99999999999L/138), 99999999999L/137+11);
-		for (long prime : primeListBetween) {
-			count++;
-			/**
-			 * maa finne ut en maate aa finne siste digits paa
-			 * 
-			 * kan kanskje gange med x -> prime og se om de frste tallene er lik 567890..0137..
-			 */
-			if (isCyclic(prime)) {
-				System.out.println(prime + " " + count+"/"+primeListBetween.size() + " "+t);
-			}
-			
-		}
-		System.out.println(count);
-	}
-	
-	private static boolean isCyclic(long p) {
-		long count = 0;
-		long r = 1;
-		long lastDigits = 0;
-		do {
-			count++;
-			long x = r*10;
-			long lastDigit = x/p;
-			r = x % p;
-			lastDigits = (lastDigits*10 + lastDigit)%100000;
-		} while(r != 1);
-		
-		if (count == p-1) {
-			System.out.println(lastDigits);
-			if (lastDigits == 56789) {
-				/**
-				 * dette er maalet
-				 */
-				System.out.println(p + "!!!!!!!!!!!!!!!!!!!!!!!!");
-				System.exit(0);
-			}
-		}
-		return (count == p-1);
-	}
+    public static void main(String[] args) {
+        T t = new T();
+        for (long prime : Euler.primeListBetween((99999999999L / 138), 99999999999L / 137 + 11)) {
+            if (lastDigits(prime) == 56789L) {
+                System.out.println(prime);
+                if (isCyclic(prime)) {
+                    System.out.println(cyclicSum(prime) + " " + t);
+                }
+            }
+        }
+    }
+
+    private static long lastDigits(long p) {
+        long lastDigits = 0;
+        for (long i = p - 6; i < p - 1; i++) {
+            lastDigits = lastDigits * 10 + (10 * (valueOf(10).modPow(valueOf(i), valueOf(p)).longValue())) / p;
+        }
+        return lastDigits;
+    }
+
+    private static long cyclicSum(long p) {
+        long sum = 0;
+        long r = 1;
+        do {
+            long x = r * 10;
+            sum += x / p;
+            r = x % p;
+        } while (r != 1);
+        return sum;
+    }
+
+    private static boolean isCyclic(long p) {
+        long count = 0;
+        long r = 1;
+        do {
+            count++;
+            long x = r * 10;
+            r = x % p;
+        } while (r != 1);
+        return (count == p - 1);
+    }
 }
