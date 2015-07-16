0e4b556c480009d811e4d11b3678aca664a2980a
Magnus S. Thrap <magnus.solheim@gmail.com>
2015-07-16 14:37:53 +0200

    Solve #521 base case

---
+++ src/Java/Problem521BaseCase.java
@@ -0,0 +1,23 @@
+package Java;
+
+public class Problem521BaseCase {
+    public static void main(String[] args) {
+        System.out.println(S(100));
+    }
+
+    private static long S(int n) {
+        long S = 0;
+        boolean[] smpf = new boolean[n+1];
+        for (int i = 2; i <= n; i++) {
+            if (!smpf[i]) {
+                for (int j = 1; j*i <= n; j++) {
+                    if (!smpf[i*j]) {
+                        smpf[i*j] = true;
+                        S += i;
+                    }
+                }
+            }
+        }
+        return S;
+    }
+}
