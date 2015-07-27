8b2f400f03fd1ac78024c8964ab22e939d66d52c
Magnus S. Thrap <magnus.solheim@gmail.com>
2015-07-27 11:19:00 +0200

    Solve #335 base case

---
+++ src/Java/Problem335BaseCase.java
@@ -0,0 +1,30 @@
+package Java;
+
+import java.util.Arrays;
+
+public class Problem335BaseCase {
+    public static void main(String[] args) {
+        System.out.println(M(5));
+        System.out.println(M(100));
+    }
+
+    private static int M(int x) {
+        int[] bowls = new int[x];
+        Arrays.fill(bowls, 1);
+
+        int bowl = 0;
+        for (int i = 0; true; i++) {
+            if (x <= 10)
+                System.out.println(Arrays.toString(bowls));
+
+            if (bowls[bowl % x] == x)
+                return i+1;
+            int lastBowl = bowl % x;
+            while (bowls[lastBowl] != 0) {
+                bowl++;
+                bowls[bowl % x]++;
+                bowls[lastBowl]--;
+            }
+        }
+    }
+}
