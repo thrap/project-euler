c8012bee805a0eb3e17270acc80f1f97aacdb15c
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2020-08-19 06:59:03 +0200

    Refactor p(L, n)

--- src/main/kotlin/thrap/euler/Problem686.kt
+++ src/main/kotlin/thrap/euler/Problem686.kt
@@ -2,20 +2,23 @@ package thrap.euler
 
 import kotlin.math.floor
 import kotlin.math.log
-import kotlin.math.log10
 import kotlin.math.pow
 
 fun main() {
-    tailrec fun p(L: Int, n: Int, j: Int = 1, count: Int = 0): Int {
-        if (count == n)
-            return j - 1
-
+    fun p(L: Int, n: Int): Int {
         val digits = "$L".length
 
-        val x = j * log(2.0, 10.0);
-        val y = floor(10.0.pow(x - floor(x) + digits - 1)).toInt();
+        tailrec fun rec(j: Int, count : Int): Int =
+            if (count == n)
+                j - 1
+            else {
+                val x = j * log(2.0, 10.0);
+                val y = floor(10.0.pow(x - floor(x) + digits - 1)).toInt();
 
-        return p(L, n, j + 1, count + if (y == L) 1 else 0)
+                rec(j+1, count + if (y == L) 1 else 0)
+            }
+
+        return rec(1, 0)
     }
 
     println(p(123, 45))
